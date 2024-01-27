781c4df7291584cd7588790b7d8024c8b180e306
Taishi-N324 <82321333+Taishi-N324@users.noreply.github.com>
2024-01-27 16:57:44 +0900

    Unnecessary script deletion

--- scripts/get.sh
+++
@@ -1,4 +0,0 @@
-
-
-
-python main.py   --tasks mbpp  --allow_code_execution  --load_generations_path /bb/llm/gaf51275/llm-jp/taishi-work-space/bigcode-evaluation-harness/results/generations/aurora-m/Aurora-90k-hf_50/mbpp_conala.json  --model aurora-m/Aurora-90k-hf
