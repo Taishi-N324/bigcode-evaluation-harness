048985aff4092f0265885dab04429bc9c0d05a55
Taishi Nakamura <82321333+Taishi-N324@users.noreply.github.com>
2024-02-01 03:22:41 +0900

    Code to be evaluated collectively

--- scripts/wsl/eval-all.sh
+++ scripts/wsl/eval-all.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-bash scripts/wsl/eval-mult.sh \
-    aurora-m/aurora-103.9k-hf \
-    multiple-cpp \
-    /bigcode-evaluation-harness/results/generations/aurora-m/Aurora-103.9k-hf_1n_samples_Falsedo_sample_0.8_temperature_eval/multiple-cpp_multiple-cpp.json \
+models=(
+    "aurora-m/aurora-103.9k-hf"
+    "aurora-m/aurora-102k-hf"
+    "aurora-m/aurora-100k-hf"
+    "aurora-m/aurora-97.5k-hf"
+    "aurora-m/aurora-95k-hf"
+    "aurora-m/aurora-90k-hf"
+    "aurora-m/aurora-80k-hf"
+    "aurora-m/aurora-70k-hf"
+    "aurora-m/aurora-60k-hf"
+    "aurora-m/aurora-50k-hf"
+    "aurora-m/aurora-40k-hf"
+    "aurora-m/aurora-30k-hf"
+    "aurora-m/aurora-20k-hf"
+    "aurora-m/aurora-10k-hf"
+    "bigcode/starcoderbase"
+    "bigcode/starcoderbase"
+    "bigcode/starcoderplus"
+)   
+
+languages=("cpp" "cs" "java" "php" "sh" "ts")
+
+for model_name in "${models[@]}"; do
+    for lang in "${languages[@]}"; do
+        echo "Evaluating model ${model_name} with language ${lang}"
+        bash scripts/wsl/eval-mult.sh \
+            $model_name \
+            multiple-${lang} \
+            /bigcode-evaluation-harness/results/generations/${model_name}_1n_samples_Falsedo_sample_0.8_temperature_eval/multiple-${lang}_multiple-${lang}.json
+    done
+done
