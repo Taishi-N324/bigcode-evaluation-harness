16fae7a7965fadd37ef24bf1e07dff70358e4ccf
Taishi-N324 <taishiahiru5@gmail.com>
2024-02-01 03:10:39 +0900

    For eval

---
+++ scripts/wsl/eval-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash scripts/wsl/eval-mult.sh \
+    aurora-m/aurora-103.9k-hf \
+    multiple-cpp \
+    /bigcode-evaluation-harness/results/generations/aurora-m/Aurora-103.9k-hf_1n_samples_Falsedo_sample_0.8_temperature_eval/multiple-cpp_multiple-cpp.json \
---
+++ scripts/wsl/eval-mult.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+model_name=$1
+task=$2
+load_generations_path_name=$3
+
+n_samples=1
+do_sample=False
+temperature=0.8
+
+mkdir -p /home/taishi/workspace/code/bigcode-evaluation-harness/results/metrics/${model_name}_${n_samples}n_samples_${do_sample}do_sample_${temperature}_temperature_eval
+
+sudo docker run -v \
+    /home/taishi/workspace/code/bigcode-evaluation-harness:/bigcode-evaluation-harness:ro \
+    -v /home/taishi/workspace/code/bigcode-evaluation-harness/results/metrics:/metrics \
+    -it ghcr.io/bigcode-project/evaluation-harness-multiple:latest \
+    python3 main.py \
+    --model $model_name \
+    --tasks $task \
+    --load_generations_path $load_generations_path_name \
+    --allow_code_execution \
+    --temperature $temperature \
+    --precision bf16 \
+    --do_sample $do_sample \
+    --top_p 0.95 \
+    --max_length_generation 512 \
+    --n_samples $n_samples \
+    --metric_output_path /metrics/${model_name}_${n_samples}n_samples_${do_sample}do_sample_${temperature}_temperature_eval/${task}.json \
